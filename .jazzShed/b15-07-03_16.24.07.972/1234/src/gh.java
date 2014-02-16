af3ddd893e185f08c49ad1f2d49645b91e2c8361
harshvardhanibm <harsvsin@in.ibm.com>
2014-02-16 14:00:00 +0100

    Share

---
+++ .jazzShed/b15-07-03_16.24.07.972/1234/src/gh.java
@@ -0,0 +1,7 @@
+
+public class gh {
+private void Syso() {
+	// TODO Auto-generated method stub
+
+}
+}
---
+++ SCM Java/src/SCMTest.java
@@ -0,0 +1,10 @@
+
+public class SCMTest {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hi");
+		System.out.println("hi subhro");
+		System.out.println("Hi Harsh");
+	}
+}
